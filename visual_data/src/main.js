72c39b98e61a3aa67cbc5dc319e19d8aab7c2c87
licong <congli_113@sina.com>
2018-07-13 17:10:49 +0800

    检测输入数据，添加排序功能

--- visual_data/src/main.js
+++ visual_data/src/main.js
@@ -1,18 +1,22 @@
-
 var createTable = function (){
-    var num = 4;
-    var data = {
-        "apple": 5,
-        pen: 3, 
-        pineapple: 10,
-        grape: 8
+    // 清除页面上的错误提示语和图表
+    document.getElementById("tip").innerText = "";
+    var chartEle = document.getElementById("chart");
+    var divEle = chartEle.getElementsByTagName("div")[0];
+    if(divEle){
+        chartEle.removeChild(divEle);
+    }
+
+    var data = getData();
+    if(!data){
+        return;
     }
 
     var maxKeyLength = 0, maxValue = 0;
-    for(var key in data){
-        maxKeyLength = key.length > maxKeyLength ? key.length : maxKeyLength;
-        maxValue = data[key] > maxValue ? data[key] : maxValue;
-    }
+    data.forEach( item => {
+        maxKeyLength = item.name.length > maxKeyLength ? item.name.length : maxKeyLength;
+        maxValue = item.value > maxValue ? item.value : maxValue;
+    })
 
     var i = 0, spaceStr = "", barStr = "";
     while(true){
@@ -27,22 +31,19 @@ var createTable = function (){
     var output = [];
     output.push(getBorder(1, maxKeyLength));
     var i = 0;
-    for(var key in data){
-        var keySpaceLen = maxKeyLength - key.length;
-        var barLen = Math.floor((data[key]/maxValue)*20);
+    data.forEach(item => {
+        var keySpaceLen = maxKeyLength - item.name.length;
+        var barLen = Math.floor((item.value/maxValue)*20);
         var valueSpaceLen = 20 - barLen;
-        var str = "&#x02502;" + spaceStr.substr(0, keySpaceLen*9) + key + "&#x02502;" + barStr.substr(0, barLen*9) + spaceStr.substr(0, valueSpaceLen*9) + "&#x02502;";
+        var str = "&#x02502;" + spaceStr.substr(0, keySpaceLen*9) + item.name + "&#x02502;" + barStr.substr(0, barLen*9) + spaceStr.substr(0, valueSpaceLen*9) + "&#x02502;";
         output.push(str);
-        if(++i < num){
+        if(++i < data.length){
             output.push(getBorder(3, maxKeyLength));
         }
-    }
-
+    })
     output.push(getBorder(2, maxKeyLength));
 
-    var chartEle = document.getElementById("chart");
     var div = document.createElement("div");
-
     for(var i=0; i < output.length; i++){
         var ele = document.createElement("p");
         ele.innerHTML = output[i];
@@ -50,12 +51,6 @@ var createTable = function (){
     }
     chartEle.appendChild(div);
 }
-
-// “┌”（\u250c），“┐”（\u2510），“└”（\u2514），“┘”（\u2518）：图表外框转角符号
-// “─”（\u2500），“│”（\u2502）：图表中的横、竖线
-// “├”（\u251c），“┤”（\u2524），“┬”（\u252c），“┴”（\u2534），“┼”（\u253c）：图表中的各种交叉线
-// “█”（\u2588）：用来拼柱子的字符
-// “ ”（\u0020）：图表中的空格
 
 // type 边框类型， 1 上边框， 2 下边框， 3 两条数据中间的分隔线
 var getBorder = function(type, keyLength){
@@ -91,6 +86,118 @@ var getBorder = function(type, keyLength){
 }
 
 var getData = function(){
-    
-}
+    var strContent = document.getElementById("data").value;
+    if(!strContent){
+        return;
+    }
+    strContent = strContent.replace(/\r/g, ""); // IE9以上、FF、chrome的换行为\n， IE7-8的时候换行为\r\n
+    strContent = strContent.replace(/\n\s*\n/g, "\n");  // 合并掉连续的换行
+    var rows = strContent.split("\n");
 
+    if(rows[rows.length-1].trim() == ""){
+        rows.pop();
+    }
+
+    var count = 0;
+    var data = [];
+    var orderBy; 
+    var orderDir; 
+    var isValid = true;
+    var errorMessage = "";
+
+    for(var i=0, len=rows.length; i < len; i++){
+        var item = rows[i].trim();
+        if(item == ""){
+            continue
+        }
+
+        if(i == 0){
+            if(/\D/g.test(item)){
+                isValid = false;
+                errorMessage = "请在第1行输入数据条目数N, 且1<=N<=20。";
+                break;
+            }
+
+            item = parseInt(item);
+            if(item >= 1 && item <= 20){
+                count = item;
+            }else{
+                isValid = false;
+                errorMessage = "请在第1行输入数据条目数N, 且1<=N<=20。";
+                break;
+            }
+        } else{
+            item = item.replace(/\s+/g, " "); // 把name， value之间的多个空格合并成一个
+            item = item.split(" ");
+            if(item.length == 2){
+                if(i == 1){
+                    if(item[0].trim().toLowerCase() == "name"){
+                        orderBy = "name";
+                    }else if(item[0].trim().toLowerCase() == "value"){
+                        orderBy = "value";
+                    }else{
+                        isValid = false;
+                        errorMessage = "指定的数据排序方式错误";
+                        break;
+                    }
+
+                    if(item[1].trim().toUpperCase() == "ASC"){
+                        orderDir = "ASC";
+                    }else if(item[1].trim().toUpperCase() == "DESC"){
+                        orderDir = "DESC";
+                    }else{
+                        isValid = false;
+                        errorMessage = "指定的数据排序方式错误";
+                        break;
+                    }
+                }else{
+                    // 数据条目的名称，仅包含小写字母
+                    if(/[^a-z]/g.test(item[0].trim())){
+                        isValid = false;
+                        errorMessage = "第" + (i+1) + "行数据格式错误";
+                        break;
+                    }
+
+                    if(/\D/g.test(item[1].trim())){
+                        isValid = false;
+                        errorMessage = "第" + (i+1) + "行数据格式错误";
+                        break;
+                    }
+
+                    var val = parseInt(item[1].trim());
+                    if(val < 0 || val > 1000000){
+                        isValid = false;
+                        errorMessage = "第" + (i+1) + "行数据的值超出范围， 0 <=value<= 1000000";
+                        break;
+                    }
+
+                    data.push({name: item[0], value: parseInt(item[1].trim())});
+                }
+            }else{
+                isValid = false;
+                errorMessage = "第" + (i+1) + "行数据格式错误";
+                break;
+            }
+        }
+    }
+
+    if(isValid && count != data.length){
+        isValid = false;
+        errorMessage = "输入数据与条目数不符";
+    }
+ 
+    if(!isValid){
+        document.getElementById("tip").innerText = errorMessage;
+    }else{
+        data.sort((a, b) => {
+            if(a[orderBy] > b[orderBy]){
+                return orderDir == "ASC" ? 1 : -1;
+            }else if((a[orderBy] < b[orderBy])){
+                return orderDir == "ASC" ? -1 : 1;
+            }else{
+                return 0;
+            }
+        });
+        return data;
+    }
+}
